392b11f1f6d26c1395973f66d983052c1cf97617
Boris Rybalkin <ribalkin@gmail.com>
2025-05-10 13:17:42 +0100

    check

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -206,9 +206,9 @@ function authenticateLdap($request)
 
     $account = [
         'username' => $username,
-        'email' => $results[0]["mail"],
-        'first_name' => $results[0]["givenName"],
-        'last_name' => $results[0]["sn"],
+        'email' => $results[0]["mail"] ?? '',
+        'first_name' => $results[0]["givenName"] ?? '',
+        'last_name' => $results[0]["sn"] ?? '',
         'password' => base64_encode(random_bytes(20))
     ];
 
